100718554e5f1d53782022ca84e6c6c8abfa0c3e
Luan Pinto <luan.nelmo@hotmail.com>
2022-05-24 21:34:51 -0300

    Finalizado

--- src/main/java/com/nelioalves/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/UserRepository.java
@@ -1,13 +1,11 @@
-package com.luannp.workshopmongo.repositories;
+package com.nelioalves.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.luannp.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-
-	User findOne(String id);
 
 }
